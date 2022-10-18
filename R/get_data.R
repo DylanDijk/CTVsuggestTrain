9c56038a84a39174fbf79f63267d3c3f442c0eeb
Dylan Dijk <dylan_2103@hotmail.com>
2022-10-18 16:53:49 +0100

    creating get_NLP function that takes get_data output as input then returns cosine similarity between package text and task view description text

--- R/get_data.R
+++ R/get_data.R
@@ -1,14 +1,14 @@
 
 #' Title
 #'
-#' @param test
+#' @param TEST
 #' @param limiting_n_observations
 #'
 #' @return Data objects required for rest of scripts involved in training the model
 #'
 #'
 #' @examples
-get_data = function(test = FALSE, limiting_n_observations = 100){
+get_data = function(TEST = FALSE, limiting_n_observations = 100){
 
   message("Downloading package metadat from CRAN package repository")
 
@@ -25,8 +25,9 @@ get_data = function(test = FALSE, limiting_n_observations = 100){
 
   ############ TESTING ###########
   # Limits number of observations in dataset to speed up tests
-if(test){
-  CRAN_data = CRAN_data[sample(size = limiting_n_observations, x = nrow(CRAN_data)),]
+if(TEST){
+  test_sample = sample(size = limiting_n_observations, x = nrow(CRAN_data))
+  CRAN_data = CRAN_data[test_sample,]
 }
   ################################
 
@@ -38,7 +39,7 @@ if(test){
   CRAN_cranly_data = cranly::clean_CRAN_db(packages_db = CRAN_data)
 
 
-  return(list("CRAN_data" = CRAN_data, "all_CRAN_pks" = all_CRAN_pks, "CRAN_cranly_data" = CRAN_cranly_data, "tvdb" = tvdb))
+  return(list("CRAN_data" = CRAN_data, "all_CRAN_pks" = all_CRAN_pks, "CRAN_cranly_data" = CRAN_cranly_data, "tvdb" = tvdb, "TEST" = TEST))
 
 }
 
